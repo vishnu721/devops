5bc0564b3f65febe94c22efc15deeea9da551f3a
Vishnu S <vishnu721@gmail.com>
2021-09-06 10:46:02 +0530

    update

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "tls_private_key" "ec2_private_key" {
     algorithm = "RSA"
     rsa_bits  = 4096
     provisioner "local-exec" {
-      command = "echo '${tls_private_key.ec2_private_key.private_key_pem}' > ${terraform.workspace}/${var.key_name}.pem"
+      command = "echo '${tls_private_key.ec2_private_key.private_key_pem}' > ${terraform.workspace}/${aws_instance.webserver.key_name}.pem"
     }
 }
 
@@ -27,6 +27,6 @@ resource "null_resource" "key-perm" {
       tls_private_key.ec2_private_key,
     ]
     provisioner "local-exec" {
-      command = "chmod 400 ${terraform.workspace}/${var.key_name}.pem"
+      command = "chmod 400 ${terraform.workspace}/${aws_instance.webserver.key_name}.pem"
     }
 }
